87cdf9bc4725117b491829820fede2612623c12e
Banx87 <joni.lindholm@gmail.com>
2025-04-04 14:05:03 +0300

    [ADD] integrate FakeWeatherFetcher and update view rendering with weather info; add new SVG icons for weather conditions

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
+
 require __DIR__ . '/inc/all.inc.php';
 
+require_once __DIR__ . '/src/Weather/FakeWeatherFetcher.php';
+$fetcher = new FakeWeatherFetcher();
+$info = $fetcher->fetch('Los Angeles');
 
 render(
 	'index.view',
-	[]
+	['fetcher' => $fetcher, 'info' => $info]
 );
